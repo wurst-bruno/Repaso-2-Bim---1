0d33df7eceb37b161f09d700d9d4a2b8e96387cd
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-05-17 15:56:31 -0300

    nunca anduvo

---
+++ Assets/Script/BrickCloner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+
+    public GameObject brickPrefab;
+    public float xOffset;
+    public Text userOffset;
+    int counter = 0;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CloneBrick()
+    {
+        if(counter == 0)
+        {
+            xOffset = float.Parse(userOffset.text);
+        }
+
+
+        if (counter < 5)
+        {
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            clon.transform.position = new Vector3(xOffset, 0, 0);
+            xOffset += 1.1f;
+            counter++;
+
+        }
+
+    }
+
+
+}
+
